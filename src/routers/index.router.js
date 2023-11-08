73d6cd7bde4a01529d447fdd3e64591ad7f6c5b3
Hải Nguyễn <nvhai2408@gmail.com>
2023-11-09 00:01:40 +0700

    update vercel.json to deploy

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -4,6 +4,9 @@ const uploadImageRouter = require("./image.router");
 function Routers(app) {
   app.use("/user", userRouter);
   app.use("/upload", uploadImageRouter);
+  app.get("/", (req, res) => {
+    res.send({ message: "Deploy Ok!" });
+  });
 }
 
 module.exports = Routers;
